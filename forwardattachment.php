617dd69d22c27cf17e63f84910f9bda1da9702b1
PhilW <roundcube@tehinterweb.co.uk>
2011-11-20 18:53:08 +0000

    use core attach function

--- forwardattachment.php
+++ forwardattachment.php
@@ -36,40 +36,65 @@ class forwardattachment extends rcube_plugin
 
 			$uids = explode(",", $_POST['_uid']);
 			foreach ($uids as $key => $uid) {
-				$file = tempnam($temp_dir, 'emlattach');
-				$message = $imap->get_raw_body($uid);
-				$headers = $imap->get_headers($uid);
-				$subject = $imap->decode_header($headers->subject);
-				$subject = substr($subject, 0, 16);
-
-				if (isset($subject) && $subject != "")
-					$disp_name = $subject . ".eml";
-				else
-					$disp_name = "message_rfc822.eml";
-
-				if (file_put_contents($file, $message)) {
-					$attachment = array(
-						'path' => $file,
-						'size' => filesize($file),
-						'name' => $disp_name,
-						'mimetype' => "message/rfc822"
-						);
-
-					// save attachment if valid
-					if (($attachment['data'] && $attachment['name']) || ($attachment['path'] && file_exists($attachment['path'])))
-						$attachment = $rcmail->plugins->exec_hook('attachment_save', $attachment);
-
-					if ($attachment['status'] && !$attachment['abort']) {
-						unset($attachment['data'], $attachment['status'], $attachment['abort']);
-						$_SESSION['compose']['attachments'][] = $attachment;
-					}
-				}
+				$message = new rcube_message($uid);
+				$this->_rcmail_write_forward_attachment($message);
 			}
 		}
 
 		$_SESSION['compose']['param']['sent_mbox'] = $rcmail->config->get('sent_mbox');
 		$rcmail->output->redirect(array('_action' => 'compose', '_id' => $_SESSION['compose']['id']));
 		exit;
+	}
+
+	// Creates an attachment from the forwarded message
+	// Copied from program/steps/mail/compose.inc
+	private function _rcmail_write_forward_attachment(&$message)
+	{
+		$rcmail = rcmail::get_instance();
+
+		if (strlen($message->subject))
+			$name = mb_substr($message->subject, 0, 64) . '.eml';
+		else
+			$name = 'message_rfc822.eml';
+
+		$mem_limit = parse_bytes(ini_get('memory_limit'));
+		$curr_mem = function_exists('memory_get_usage') ? memory_get_usage() : 16*1024*1024; // safe value: 16MB
+		$data = $path = null;
+
+		// don't load too big attachments into memory
+		if ($mem_limit > 0 && $message->size > $mem_limit - $curr_mem) {
+			$temp_dir = unslashify($rcmail->config->get('temp_dir'));
+			$path = tempnam($temp_dir, 'rcmAttmnt');
+			if ($fp = fopen($path, 'w')) {
+				$rcmail->imap->get_raw_body($message->uid, $fp);
+				fclose($fp);
+			}
+			else {
+				return;
+			}
+		}
+		else {
+			$data = $rcmail->imap->get_raw_body($message->uid);
+		}
+
+		$attachment = array(
+			'group' => $_SESSION['compose']['id'],
+			'name' => $name,
+			'mimetype' => 'message/rfc822',
+			'data' => $data,
+			'path' => $path,
+			'size' => $path ? filesize($path) : strlen($data),
+		);
+
+		$attachment = $rcmail->plugins->exec_hook('attachment_save', $attachment);
+
+		if ($attachment['status']) {
+			unset($attachment['data'], $attachment['status'], $attachment['content_id'], $attachment['abort']);
+			$_SESSION['compose']['attachments'][$attachment['id']] = $attachment;
+		}
+		elseif ($path) {
+			@unlink($path);
+		}
 	}
 }
 
